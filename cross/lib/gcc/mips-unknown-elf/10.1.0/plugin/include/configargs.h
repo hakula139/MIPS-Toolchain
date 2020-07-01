ed8b7bc7ef3c458037cd5d1a30aa969854fd7ed9
Hakula Chen <i@hakula.xyz>
2020-07-02 06:26:05 +0800

    Set up cross-compiler
    mips-unknown-elf

---
+++ cross/lib/gcc/mips-unknown-elf/10.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../src/gcc-10.1.0/configure CFLAGS='-O3 -march=native' CXXFLAGS='-O3 -march=native' --prefix=/mnt/e/Github/MIPS-U-Boot/cross --target=mips-unknown-elf --enable-thumb --enable-interwork --enable-multilib --enable-languages=c --with-system-zlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
